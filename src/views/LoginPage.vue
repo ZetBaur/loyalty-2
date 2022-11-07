c7831dc495473baa6de26a1470b6b6fc5d48abec
Ruslan <galinskirus@gmail.com>
2022-11-07 11:55:26 +0600

    next

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,55 @@
+<template>
+    <el-card class="box-card">
+        <el-form label-width="70px">
+            <el-form-item label="Login">
+                <el-input v-model="formInfo.login" />
+            </el-form-item>
+
+            <el-form-item label="Пароль">
+                <el-input v-model="formInfo.password" />
+            </el-form-item>
+
+            <el-form-item>
+                <el-button
+                    type="primary"
+                    @click="submit"
+                    :disabled="!btnEnabled"
+                    >Войти</el-button
+                >
+            </el-form-item>
+        </el-form>
+    </el-card>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+// import { useLoginStore } from '@/stores/auth/loginStore';
+import { computed } from 'vue';
+
+// const loginStore = useLoginStore();
+
+const formInfo = ref({
+    login: '',
+    password: ''
+});
+
+const btnEnabled = computed(() => {
+    return !!formInfo.value.login && !!formInfo.value.password;
+});
+
+const submit = () => console.log('login');
+</script>
+
+<style scoped>
+.text {
+    font-size: 14px;
+}
+
+.item {
+    padding: 18px 0;
+}
+
+.box-card {
+    width: 300px;
+}
+</style>
