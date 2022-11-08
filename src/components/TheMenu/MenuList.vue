1f76b4af0dc431a5d645d945926275afb43ce7a9
Ruslan <galinskirus@gmail.com>
2022-11-08 12:36:29 +0600

    next

---
+++ src/components/TheMenu/MenuList.vue
@@ -0,0 +1,46 @@
+<template>
+    <ul class="menu-list">
+        <SingleMenuItem link="Страница 1" path="/" />
+
+        <MenuItems
+            itemName="Страница 2"
+            path="/"
+            :links="[
+                {
+                    link: 'Страница 2-1',
+                    path: '/page'
+                },
+                {
+                    link: 'Страница 2-2',
+                    path: '/page2'
+                }
+            ]"
+        />
+    </ul>
+</template>
+
+<script setup lang="ts">
+import MenuItems from './MenuItems.vue';
+import SingleMenuItem from './SingleMenuItem.vue';
+</script>
+
+<style lang="scss">
+.menu-list {
+    background: $custom-black;
+    color: #ffffff;
+    width: auto;
+    white-space: nowrap;
+    display: flex;
+    flex-direction: column;
+    user-select: none;
+
+    & li {
+        padding: 25px 0;
+    }
+
+    & .el-icon,
+    & a {
+        color: #ffffff;
+    }
+}
+</style>
--- src/components/TheMenu/TheMenu.vue
+++ src/components/TheMenu/TheMenu.vue
@@ -4,55 +4,13 @@
             <LogoIcon />
         </div>
 
-        <ul class="menu-list">
-            <SingleMenuItem link="Страница 1" path="/" />
-
-            <MenuItems
-                itemName="Страница 2"
-                path="/"
-                :links="[
-                    {
-                        link: 'Страница 2-1',
-                        path: '/page'
-                    },
-                    {
-                        link: 'Страница 2-2',
-                        path: '/page2'
-                    }
-                ]"
-            />
-
-            <!-- ---------------------------------- -->
-
-            <!-- <li>
-                <RouterLink to="/"> Страница 2 </RouterLink>
-            </li>
-
-            <li>
-                <div class="item-title" @click="toggle">
-                    <div>Страница 3</div>
-
-                    <ArrowIcon class="arrow" :class="{ expanded: expanded }" />
-                </div>
-
-                <ul class="nested-list" v-if="expanded">
-                    <li>
-                        <RouterLink to="/"> Страница 3-1 </RouterLink>
-                    </li>
-
-                    <li>
-                        <RouterLink to="/"> Страница 3-2 </RouterLink>
-                    </li>
-                </ul>
-            </li> -->
-        </ul>
+        <MenuList />
     </div>
 </template>
 
 <script setup lang="ts">
-import MenuItems from './MenuItems.vue';
 import LogoIcon from '../icons/LogoIcon.vue';
-import SingleMenuItem from './SingleMenuItem.vue';
+import MenuList from './MenuList.vue';
 </script>
 
 <style lang="scss">
@@ -63,24 +21,5 @@ import SingleMenuItem from './SingleMenuItem.vue';
 
 .logo {
     text-align: center;
-}
-
-.menu-list {
-    background: $custom-black;
-    color: #ffffff;
-    width: auto;
-    white-space: nowrap;
-    display: flex;
-    flex-direction: column;
-    user-select: none;
-
-    & li {
-        padding: 25px 0;
-    }
-
-    & .el-icon,
-    & a {
-        color: #ffffff;
-    }
 }
 </style>
