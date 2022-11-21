057e477c93a7f73707216c903878b54f3147f67e
Ruslan <galinskirus@gmail.com>
2022-11-21 16:27:51 +0600

    next

--- src/views/vueflow/FlowPage.vue
+++ src/views/vueflow/FlowPage.vue
@@ -109,9 +109,6 @@ onMounted(() => {
 </script>
 
 <style>
-/* import the required styles */
 @import '@vue-flow/core/dist/style.css';
-
-/* import the default theme (optional) */
 @import '@vue-flow/core/dist/theme-default.css';
 </style>
