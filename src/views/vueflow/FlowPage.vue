75cc04504dc8dc4b8ffa5bc7e548d17f5322e339
Ruslan <galinskirus@gmail.com>
2022-11-21 15:06:58 +0600

    next

--- src/views/vueflow/FlowPage.vue
+++ src/views/vueflow/FlowPage.vue
@@ -1,4 +1,16 @@
 <template>
+    <HeaderView />
+
+    <div class="flex it mt-4">
+        <BaseButton @click="onAdd" text="Конверсия" class="mr-4">
+            <ConversionIcon />
+        </BaseButton>
+
+        <BaseButton text="Этапы">
+            <StageIcon />
+        </BaseButton>
+    </div>
+
     <VueFlow
         v-model="elements"
         class="basicflow"
@@ -8,23 +20,94 @@
         :max-zoom="4"
         fit-view-on-init
     >
-        <!-- <Background pattern-color="#aaa" gap="8" /> -->
+        <template #node-custom="{ data }">
+            <CustomNode :data="data" />
+        </template>
     </VueFlow>
 </template>
 
 <script setup>
-import { VueFlow, useVueFlow } from '@vue-flow/core';
-import { ref } from 'vue';
-import { initialElements } from './initial-elements.ts';
+import { VueFlow, useVueFlow, Position } from '@vue-flow/core';
+import { ref, onMounted } from 'vue';
+// import { initialElements } from './initial-elements.ts';
+import HeaderView from './components/HeaderView.vue';
+import BaseButton from '@/components/ui/BaseButton.vue';
+import ConversionIcon from '@/components/icons/ConversionIcon.vue';
+import StageIcon from '@/components/icons/StageIcon.vue';
+import CustomNode from './nodes/CustomNode.vue';
 
 const { onConnect, addEdges } = useVueFlow();
 
-const elements = ref(initialElements);
+const {
+    nodes,
+    addNodes
+    // setNodes,
+    // setEdges,
+    // dimensions,
+    // setTransform,
+    // toObject
+} = useVueFlow();
+
+const elements = ref([]);
 
 onConnect((params) => {
-    console.log('fffffffffff');
+    console.log('onConnect', params);
 
     addEdges([params]);
+});
+
+const onAdd = () => {
+    const id = nodes.value.length + 1;
+
+    const newNode = {
+        type: 'custom',
+        id: id,
+        label: `Node ${id}`,
+
+        position: { x: 0, y: 0 },
+        targetPosition: Position.Left
+    };
+
+    addNodes([newNode]);
+};
+
+onMounted(() => {
+    elements.value = [
+        {
+            id: '1',
+            type: 'custom',
+            data: {},
+            position: { x: 0, y: 50 }
+        },
+        {
+            id: '2',
+            type: 'output',
+            position: { x: 350, y: 25 },
+            targetPosition: Position.Left
+        },
+        {
+            id: '3',
+            type: 'output',
+            position: { x: 350, y: 200 },
+            targetPosition: Position.Left
+        },
+        {
+            id: 'e1a-2',
+            source: '1',
+            sourceHandle: 'a',
+            target: '2',
+            animated: true,
+            style: () => ({})
+        },
+        {
+            id: 'e1b-3',
+            source: '1',
+            sourceHandle: 'b',
+            target: '3',
+            animated: true,
+            style: () => ({})
+        }
+    ];
 });
 </script>
 
---
+++ src/views/vueflow/components/HeaderView.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="flex items-center justify-between mb-4">
+        <div class="flex items-center">
+            <el-icon
+                @click="router.go(-1)"
+                class="mr-7 cursor-pointer"
+                :size="25"
+                ><ArrowLeft
+            /></el-icon>
+
+            <h1 class="text-[30px] font-bold mr-10">Новый сценарий</h1>
+
+            <EditIcon class="cursor-pointer" />
+        </div>
+
+        <div class="flex items-center">
+            <BaseButton
+                class="mx-2"
+                text="Сохранить"
+                color="green"
+                @action="emit('save')"
+            />
+            <BaseButton @action="router.go(-1)" class="mx-2" text="Отменить" />
+        </div>
+    </div>
+    <hr />
+</template>
+
+<script setup lang="ts">
+import EditIcon from '@/components/icons/EditIcon.vue';
+import BaseButton from '@/components/ui/BaseButton.vue';
+import { useRouter } from 'vue-router';
+
+const emit = defineEmits<{
+    (e: 'save'): void;
+}>();
+
+const router = useRouter();
+</script>
