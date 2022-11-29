30d503e2ad5b2575b4b1c117465812351c950c6a
Ruslan <galinskirus@gmail.com>
2022-11-29 16:32:57 +0600

    next

--- src/views/actions/components/TriggersBlock.vue
+++ src/views/actions/components/TriggersBlock.vue
@@ -1,5 +1,5 @@
 <template>
-    <h2 class="mb-3">Триггеры</h2>
+    <h2 class="mb-3 font-bold text-[24px]">Триггеры</h2>
 
     <el-select v-model="value" placeholder="Select" class="mb-5">
         <el-option
@@ -14,7 +14,7 @@
 
     <div class="flex items-center justify-between mb-5 max-w-[900px]">
         <div class="flex">
-            <h3 class="mr-5">Магазин</h3>
+            <h3 class="mr-5 font-bold">Магазин</h3>
             <el-icon class="mt-1 cursor-pointer"><Plus /></el-icon>
         </div>
         <el-icon class="mt-1 cursor-pointer"><Minus /></el-icon>
@@ -49,7 +49,7 @@
 
     <div class="flex items-center justify-between mb-5 max-w-[900px]">
         <div class="flex">
-            <h3 class="mr-5">Типы карт</h3>
+            <h3 class="mr-5 font-bold">Типы карт</h3>
             <el-icon class="mt-1 cursor-pointer"><Plus /></el-icon>
         </div>
         <el-icon class="mt-1 cursor-pointer"><Minus /></el-icon>
@@ -68,6 +68,44 @@
     </div>
 
     <hr class="mb-3" />
+
+    <div class="flex items-center justify-between mb-5 max-w-[900px]">
+        <div class="flex">
+            <h3 class="mr-5 font-bold">Условия по сумме чека</h3>
+            <el-icon class="mt-1 cursor-pointer"><Plus /></el-icon>
+        </div>
+        <el-icon class="mt-1 cursor-pointer"><Minus /></el-icon>
+    </div>
+
+    <div class="flex items-center mb-5">
+        <div class="mr-5">
+            <span class="mr-3"> Минимальная сумма чека:</span>
+            <el-select v-model="value" placeholder="Select">
+                <el-option
+                    v-for="item in options"
+                    :key="item"
+                    :label="item"
+                    :value="item"
+                />
+            </el-select>
+        </div>
+
+        <div>
+            <span class="mr-3"> ID:</span>
+            <el-select v-model="value" placeholder="Select">
+                <el-option
+                    v-for="item in options"
+                    :key="item"
+                    :label="item"
+                    :value="item"
+                />
+            </el-select>
+        </div>
+    </div>
+
+    <el-checkbox v-model="checked2" label="Option 2" />
+
+    <hr class="mb-3" />
 </template>
 
 <script setup lang="ts">
@@ -76,6 +114,8 @@ import { ref } from 'vue';
 const options = ref(['test', 'test1']);
 
 const value = ref('');
+
+const checked2 = ref(false);
 
 const dynamicTags = ref(['LongTag 1', 'Tag 2', 'Tag 3']);
 
