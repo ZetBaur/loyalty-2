13e0e2c51fc478294c64840301bcb79135e7a36c
Ruslan <galinskirus@gmail.com>
2022-11-30 15:54:18 +0600

    next

---
+++ src/views/actions/components/triggers/ActorSection.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="flex items-center justify-between max-w-[900px] mb-4">
+        <div class="flex items-center">
+            <h3>Магазин</h3>
+
+            <el-icon class="ml-3 cursor-pointer"><Plus /></el-icon>
+        </div>
+
+        <el-icon class="cursor-pointer"><Minus /></el-icon>
+    </div>
+
+    <el-form>
+        <el-form-item label="ID">
+            <el-select
+                v-model="form.region"
+                placeholder="please select your zone"
+            >
+                <el-option
+                    v-for="item in options"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                />
+            </el-select>
+        </el-form-item>
+
+        <el-form-item class="ml-4">
+            <el-tag
+                v-for="tag in dynamicTags"
+                :key="tag"
+                class="mx-1"
+                closable
+                :disable-transitions="false"
+                @close="handleClose(tag)"
+            >
+                {{ tag }}
+            </el-tag>
+        </el-form-item>
+    </el-form>
+</template>
+
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+
+const form = reactive({
+    name: '',
+    region: '',
+    date1: '',
+    date2: '',
+    delivery: false,
+    type: [],
+    resource: '',
+    desc: ''
+});
+
+const options = [
+    {
+        value: 'Option1',
+        label: 'Option1'
+    },
+    {
+        value: 'Option2',
+        label: 'Option2'
+    },
+    {
+        value: 'Option3',
+        label: 'Option3'
+    },
+    {
+        value: 'Option4',
+        label: 'Option4'
+    },
+    {
+        value: 'Option5',
+        label: 'Option5'
+    }
+];
+
+const dynamicTags = ref(['Tag 1', 'Tag 2', 'Tag 3']);
+
+const handleClose = (tag: string) => {
+    dynamicTags.value.splice(dynamicTags.value.indexOf(tag), 1);
+};
+</script>
---
+++ src/views/actions/components/triggers/TriggersSection.vue
@@ -0,0 +1,24 @@
+<template>
+    <h2 class="text-lg font-medium mb-4">Триггеры</h2>
+
+    <el-select placeholder="Select">
+        <el-option
+            v-for="item in options"
+            :key="item"
+            :label="item"
+            :value="item"
+        />
+    </el-select>
+
+    <el-divider class="border-0 bg-[#ebebeb] rounded-lg mb-10" />
+
+    <ActorSection />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import ActorSection from './ActorSection.vue';
+const options = ref(['trest', 'test2']);
+</script>
+
+<style lang="scss"></style>
